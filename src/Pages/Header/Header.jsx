5836dbcd81a61e76f602410b354aaa4cf1d3bf18
tahmedofficial <tanvirofcuo@gmail.com>
2024-05-10 11:39:54 +0600

    added navbar

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,11 +1,29 @@
-
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import { NavLink } from "react-router-dom";
 
 
 const Header = () => {
 
     return (
         <div>
-            
+            <div className="md:w-5/6 mx-auto">
+                <Navbar fluid rounded>
+                    <Navbar.Toggle className="text-black" />
+                    <h1 className="text-xl font-semibold">Colibri</h1>
+                    <div className="flex md:order-2">
+                        <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+                        <button className="bg-slate-400 px-5 rounded-lg ml-3">Login</button>
+                        <Dropdown arrowIcon={false} inline></Dropdown>
+                    </div>
+                    <Navbar.Collapse className="md:ml-auto mr-16">
+                        <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-8">
+                            <NavLink to="/" className={({ isActive }) => isActive ? "text-black bg-gray-100 px-5 py-1 rounded-lg md:text-lg font-normal" : "md:text-lg font-normal"}>Home</NavLink>
+                            <NavLink to="/allFoods" className={({ isActive }) => isActive ? "text-black bg-gray-100 px-5 py-1 rounded-lg md:text-lg font-normal" : "md:text-lg font-normal"}>All Foods</NavLink>
+                            <NavLink to="/gallery" className={({ isActive }) => isActive ? "text-black bg-gray-100 px-5 py-1 rounded-lg md:text-lg font-normal" : "md:text-lg font-normal"}>Gallery</NavLink>
+                        </div>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
         </div>
     );
 };
